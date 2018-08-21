5d81ec72ba408be1bd315055a5ca40f3220fd722
Ricardo Ribas <ricardo.ribas@marfeel.com>
2018-08-21 20:54:13 +0200

    Added template for grabbing html

--- src/index.js
+++ src/index.js
@@ -1,11 +1,31 @@
-import './../styles/index.scss';
+const DEFAULT_PORT = 3000;
 
-function component() {
-    let element = document.createElement('div');
+const express = require('express');
+const request = require('request');
+const iconv = require('iconv-lite');
+const app = express();
 
-    element.innerHTML = ['Hello', 'webpack'].join(' ');
+/* eslint-disable no-undef */
+const port = process.env.PORT || DEFAULT_PORT;
 
-    return element;
-}
+app.get('/', (req, res) => {
+  request({ encoding: null, method: 'GET', uri: 'https://www.websudoku.com/?select=1&level=1' }, (error, response, html) => {
+    try {
+      if (!error) {
+        const encodedHTML = iconv.decode(new Buffer(html), 'utf-8');
 
-document.body.appendChild(component());
+        res.send(encodedHTML);
+      } else {
+        throw new Error(error);
+      }
+    }
+    catch (error) {
+      throw new Error(error);
+    }
+  });
+});
+
+app.listen(port, function () {
+  console.log(`Listening to port ${port}`);
+});
+
