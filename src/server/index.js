ef0fc8c2a9849108052a73f77cf114db9cf59450
Ricardo Ribas <ricardo.ribas@marfeel.com>
2018-11-10 23:48:10 +0100

    Used puppeeter to scrape one html page

--- src/server/index.js
+++ src/server/index.js
@@ -1,3 +1,5 @@
+const puppeteer = require('puppeteer');
+
 const DEFAULT_PORT = 8000;
 
 const express = require('express');
@@ -8,6 +10,23 @@ const app = express();
 app.use(express.static('dist'));
 
 app.get('/api/getUsername', (req, res) => res.send({ username: os.userInfo().username }));
+
+app.get('/api/puzzle/', (req, res) => {
+	(async() => {
+		const browser = await puppeteer.launch();
+		const page = await browser.newPage();
+
+		await page.goto('https://google.com');
+
+		const $scrape = () => document.body.outerHTML;
+
+		const result = await page.evaluate($scrape);
+
+		browser.close();
+
+		res.send(result);
+	})();
+});
 
 app.listen(DEFAULT_PORT, () => {
 	console.log(`Listening to port ${DEFAULT_PORT}`); // eslint-disable-line no-console
