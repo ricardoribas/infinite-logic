b5dcac633b880b315b8b1496298efb0e1be62f2b
Ricardo Ribas <ricardo.ribas@marfeel.com>
2018-11-10 23:57:42 +0100

    Added mongoose to the project. Created and added dummy entry to the database

--- src/server/index.js
+++ src/server/index.js
@@ -1,15 +1,36 @@
-const puppeteer = require('puppeteer');
-
 const DEFAULT_PORT = 8000;
 
 const express = require('express');
+
 const os = require('os');
 
 const app = express();
 
+// const mongoose = require('mongoose');
+
+const puppeteer = require('puppeteer');
+
+// mongoose.connect('mongodb://localhost/sudoku-game');
+
+// const db = mongoose.connection;
+
+// db.on('error', () => {});
+// db.once('open', () => {});
+
+// const puzzleSchema = new mongoose.Schema({
+// 	name: String
+// });
+// const Puzzle = mongoose.model('Puzzle', puzzleSchema);
+
 app.use(express.static('dist'));
 
-app.get('/api/getUsername', (req, res) => res.send({ username: os.userInfo().username }));
+app.get('/api/getUsername', (req, res) => {
+	// const puzzle = new Puzzle({ name: os.userInfo().username });
+
+	// puzzle.save();
+
+	res.send({ username: os.userInfo().username });
+});
 
 app.get('/api/puzzle/', (req, res) => {
 	(async() => {
